3d7b0827fbcfcf417135620ebd30471fb9effc18
Master-Chaos <thomas_pointner@gmx.at>
2019-09-28 16:34:05 +0200

    Start

---
+++ main.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(int argc, char **argv)
+{
+    if((argc == 5) && (argv == ""))
+    {
+
+    }
+    else
+    {
+
+    }
+
+
+
+
+}
